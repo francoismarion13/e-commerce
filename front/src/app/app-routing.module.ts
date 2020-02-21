6fbef78b7a8fad89b2b0356ec06b9720cb3f7a5d
François MARION <francois.cavanagh@gmail.com>
2020-02-21 12:14:39 +0100

    dev panier front angular + correctif back

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'shoppingCart', 
+    children: [
+      { path: '',  component: ShoppingCartComponent },
+      { path: ':id', component: ShoppingCartComponent }
+  ]}
 ];
 
 @NgModule({
--- front/src/app/shopping-cart/shopping-cart.component.ts
+++ front/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ShoppingCartService } from '../services/shopping-cart.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor(private scS: ShoppingCartService) { }
+  myCart;
+  id;
+
+  constructor(private scS: ShoppingCartService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id') ? params.get('id') : '5e4f9e60e66c951821123cfe'
+      this.scS.getCartById(this.id).subscribe(data=>{
+        this.myCart = this.scS.calculateTotalPrice(data);
+      })
+    });
   }
-  getCart(){
-
-  }
-  valideCart(){
-
-  }
-
 }
