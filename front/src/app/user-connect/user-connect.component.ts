f9c301a2d552325f784ec31e8b081fc36651ff3b
dev-php-full-stack <lemraouiadil@gmail.com>
2020-02-27 17:27:24 +0100

    commit cart payment

--- front/src/app/user-connect/user-connect.component.ts
+++ front/src/app/user-connect/user-connect.component.ts
@@ -32,7 +32,8 @@ export class UserConnectComponent implements OnInit {
       this.scS.getCartByIdUser(this.loggedUser._id).subscribe(data => {
         sessionGlobalCart.activeCart = data;
       })
-      this.loggedUser != null ? this.uS.isLogged = true : this.uS.isLogged = false;
+     // this.loggedUser != null ? this.uS.isLogged = true : this.uS.isLogged = false;
+
       this.routes.navigate(['/userProfile/'+(sessionGlobalUser.activeUser)._id]);
     })
   }
